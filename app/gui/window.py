cd11d4f1be985237094019c49a19c9cc5e6c0800
fedoran2 <fedoran2.cvut.cz>
2026-05-23 20:07:31 +0200

    add movement of pieces and highlighting of the check

--- app/gui/window.py
+++ app/gui/window.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 import os
 from app.src.fen import parse_fen
-from app.src.pieces import Pawn
+from app.src.pieces import King
 
 IMAGES = {}
 
@@ -52,6 +52,32 @@ def draw_pieces(screen, game_board):
                 screen.blit(IMAGES[key], pygame.Rect(x * SQ_Size, y * SQ_Size, SQ_Size, SQ_Size))
 
 
+def draw_hints(screen, valid_moves):
+    for move in valid_moves:
+        y, x = move
+        center_x = x * SQ_Size + SQ_Size // 2
+        center_y = y * SQ_Size + SQ_Size // 2
+        pygame.draw.circle(screen, (169, 169, 169), (center_x, center_y), SQ_Size // 10)
+
+
+def draw_check_highlight(screen, board):
+    current_color = board.who_moves
+
+    king_y = None
+    king_x = None
+
+    for y in range(8):
+        for x in range(8):
+            if isinstance(board.board[y][x], King) and board.board[y][x].color == current_color:
+                king_y = y
+                king_x = x
+                break
+
+    if  board.is_in_check(current_color):
+        red_square_rect = pygame.Rect(king_x * SQ_Size, king_y * SQ_Size, SQ_Size, SQ_Size)
+        pygame.draw.rect(screen, (255, 0, 0), red_square_rect)
+
+
 
 
 def main():
@@ -61,14 +87,48 @@ def main():
 
     game_board = parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 
+    selected_square = None
+    valid_moves = []
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if game_board.check_game_over():
+                    continue
+
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                x = mouse_x // SQ_Size
+                y = mouse_y // SQ_Size
+
+                if selected_square:
+                    start_y, start_x = selected_square
+                    if [y, x] in valid_moves:
+                        game_board.move_piece(start_y, start_x, y, x)
+                        selected_square = None
+                        valid_moves = []
+                    else:
+                        piece = game_board.board[y][x]
+                        if piece and piece.color == game_board.who_moves:
+                            selected_square = (y, x)
+                            valid_moves = game_board.get_legal_moves(y, x)
+                        else:
+                            selected_square = None
+                            valid_moves = []
+
+                else:
+                    piece = game_board.board[y][x]
+                    if piece and piece.color == game_board.who_moves:
+                        selected_square = (y, x)
+                        valid_moves = game_board.get_legal_moves(y, x)
+
         draw_board(screen)
+        draw_check_highlight(screen, game_board)
         draw_pieces(screen, game_board)
+        draw_hints(screen, valid_moves)
         pygame.display.flip()
 
 
